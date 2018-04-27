e35c6dcbdbbb008dedd9382b6a41bfa8ba77fa29
Michal Podhradsky <mpodhradsky@galois.com>
2018-04-27 16:28:50 -0700

    Updated build script

--- setup_rust_env.sh
+++ setup_rust_env.sh
@@ -1,31 +1,19 @@
 #!/bin/bash -e
 
-TOOLCHAIN_VERSION=0.1
+TOOLCHAIN_VERSION=1.0
 
 if grep -e CentOS </etc/os-release >/dev/null 2>&1; then
-    COMPILER=rust-centos-x86_64.tar.gz
+    COMPILER=rust-centos-x86-64.tar.gz
 else
-    COMPILER=rust-x86_64.tar.gz
+    COMPILER=rust-ubuntu-x86-64.tar.gz
 fi
 
 stdbuf -oL printf "Fetching sel4-aware Rust compiler..."
 (
     rm -rf rust &&
-        mkdir rust &&
-        cd rust &&
-        curl -OL https://github.com/GaloisInc/sel4-rust/releases/download/$TOOLCHAIN_VERSION/rust.tar.gz &&
-        mv rust.tar.gz $COMPILER &&
-        tar -xf $COMPILER
-) >/dev/null 2>&1
-echo " done."
-
-stdbuf -oL printf "Fetching sel4 sysroot..."
-(
-    rm -rf sysroot &&
-        mkdir sysroot &&
-        cd sysroot &&
-        curl -OL https://github.com/GaloisInc/sel4-rust/releases/download/$TOOLCHAIN_VERSION/x86_64-sel4-robigalia.tar.gz &&
-        tar -xf x86_64-sel4-robigalia.tar.gz
+        curl -OL https://github.com/GaloisInc/sel4-rust/releases/download/$TOOLCHAIN_VERSION/$COMPILER &&
+        tar -xf $COMPILER &&
+	mv stage2 rust
 ) >/dev/null 2>&1
 echo " done."
 
@@ -34,7 +22,7 @@ mkdir -p .cargo
 cat >.cargo/config <<EOF
 [build]
 target = "x86_64-sel4-robigalia"
-rustflags = ["--sysroot", "sysroot"]
+rustflags = ["--sysroot", "rust"]
 rustc = "rust/bin/rustc"
 EOF
 echo " done."
